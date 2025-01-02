eacb581c1724d7418f13e99d734c07c843185cb4
socical <socical.dev@gmail.com>
2025-01-02 17:48:04 +0900

    Update 두 수의 나눗셈.py

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
@@ -3,8 +3,7 @@ def solution(num1, num2):
         # (num1 // num2) * 1000   형변환을 다음과 같이 쓰면 이상한 값이 나오고, 
         (num1 * 1000) // num2 하면 제대로 된 값이 나오네 ㅡㅡ 왜 일까?
         # 방식이 다양할 것 같은데,,
-        # int((num1/num2) * 1000)
-        # int((num1 * 1000) // num2) 이렇게 하면 또 되네,,,
+        # int((num1/num2) * 1000) 이렇게 하면 되네,,,
     """
     answer = (num1 * 1000) // num2
     return answer
