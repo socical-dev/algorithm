b726b41d91207a6ac164288ebe0b52ed04b201e0
socical <socical.dev@gmail.com>
2025-01-25 23:56:40 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(num_list):
+    answer = 0
+    if len(num_list) > 10:
+        answer = sum(num_list)
+    else:
+        answer = 1
+        for i in num_list:
+            answer *= i
+    return answer
