5fece066f6ed469f61256040080409c3239da527
socical <socical.dev@gmail.com>
2025-02-02 23:55:23 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,5 @@
+def solution(my_string):
+    answer = ''
+    my_string = my_string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+    answer = my_string
+    return answer
