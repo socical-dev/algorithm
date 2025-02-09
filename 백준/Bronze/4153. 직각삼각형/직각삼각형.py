1122c91f601772350656dbdb9df1afbc7091755f
socical <socical.dev@gmail.com>
2025-02-10 03:17:19 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,12 @@
+# 직각삼각형 조건: A**2 + B**2  = C**2
+
+import sys
+
+input = [v.strip() for v in sys.stdin.readlines() if v.strip() != "0 0 0"]
+
+for v in input:
+    A, B, C = sorted(map(int, v.split()))
+    if A**2 + B**2 == C**2:
+        print("right")
+    else:
+        print("wrong")
